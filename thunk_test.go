ed24ce7ca94d45d97a28fac4cb0aa7e36d659634
syuparn <s.hello.spagetti@gmail.com>
2022-01-22 09:50:46 +0900

    add test

--- thunk_test.go
+++ thunk_test.go
@@ -57,6 +57,14 @@ func TestGenerator(t *testing.T) {
 			title:   "signature with imported types",
 			dirName: "withimport",
 		},
+		/*
+			// TODO: run test once the issue is resolved
+			// https://github.com/golang/go/issues/46041
+			{
+				title:   "signature with 3rd-party imported types",
+				dirName: "withthirdimport",
+			},
+		*/
 		{
 			title:   "method signature with types defined in the same package",
 			dirName: "definedtype",
